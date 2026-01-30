6364f63fc87226946d6c64fbe7bafd69ae4bd9ef
Niall Antony <88824327+niallantony@users.noreply.github.com>
2026-01-31 08:30:51 +0900

    Wire up parser for simple expressions

--- src/main/java/com/niallantony/lox/Lox.java
+++ src/main/java/com/niallantony/lox/Lox.java
@@ -47,10 +47,13 @@ public class Lox {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
 
-        // for now, just print tokesn
-        for (Token token : tokens) {
-            System.out.println(token);
-        }
+        Parser parser = new Parser(tokens);
+        Expr expr = parser.parse();
+
+        // stop if error
+        if (hadError) return;;
+
+        System.out.println(new AstPrinter().print(expr));
     }
 
     static void error(int line, String message) {
@@ -61,5 +64,13 @@ public class Lox {
         System.err.println(
                 "[line " + line + "] Error" + where + ": " + message);
         hadError = true;
+    }
+
+    static void error(Token token, String message) {
+        if (token.type == TokenType.EOF) {
+            report(token.line, " at end", message);
+        } else {
+            report(token.line, " at '" + token.line, "'" + message);
+        }
     }
 }
