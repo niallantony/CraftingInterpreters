43154d293aaa2f9da58b57bd53ad1238b60ed66b
Niall Antony <88824327+niallantony@users.noreply.github.com>
2026-02-03 23:10:04 +0900

    Add return statemtns

--- src/main/java/com/niallantony/lox/LoxFunction.java
+++ src/main/java/com/niallantony/lox/LoxFunction.java
@@ -20,7 +20,11 @@ public class LoxFunction implements LoxCallable{
             env.define(declaration.params.get(i).lexeme, arguments.get(i));
         }
 
-        interpreter.executeBlock(declaration.body, env);
+        try {
+            interpreter.executeBlock(declaration.body, env);
+        } catch (Return returnValue) {
+            return returnValue.value;
+        }
         return null;
     }
 
