f05001e7fd8d941fc5e7e824e35f1c841c8710c5
Niall Antony <88824327+niallantony@users.noreply.github.com>
2026-02-16 15:00:47 +0900

    Start implementing scanner with single and double character lexemes

--- clox/compiler.c
+++ clox/compiler.c
@@ -3,4 +3,21 @@
 #include "scanner.h"
 #include <stdio.h>
 
-void compile(const char *source) { initScanner(source); }
+void compile(const char *source) {
+  initScanner(source);
+  for
+    line = -1;
+  for (;;) {
+    Token token = scanToken();
+    if (token.line != line) {
+      printf("%4d ", token.line);
+      line = token.line;
+    } else {
+      printf("  | ");
+    }
+    printf("%2d '%.*s'\n", token.type, token.length, token.start);
+
+    if (token.type == TOKEN_EOF)
+      break;
+  }
+}
