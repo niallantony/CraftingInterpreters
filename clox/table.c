085147588000b6b5acf92091b226091fb93e58c3
Niall Antony <88824327+niallantony@users.noreply.github.com>
2026-02-23 22:11:36 +0900

    Add deletion from hash tables

--- clox/table.c
+++ clox/table.c
@@ -22,9 +22,18 @@ void freeTable(Table *table) {
 
 static Entry *findEntry(Entry *entries, int capacity, ObjString *key) {
   uint32_t index = key->hash % capacity;
+  Entry *tombstone = NULL;
+
   for (;;) {
     Entry *entry = &entries[index];
-    if (entry->key == key || entry->key == NULL) {
+    if (entry->key == NULL) {
+      if (IS_NIL(entry->value)) {
+        return tombstone != NULL ? tombstone : entry;
+      } else {
+        if (tombstone == NULL)
+          tombstone = entry;
+      }
+    } else if (entry->key == key) {
       return entry;
     }
 
@@ -51,6 +60,8 @@ static void adjustCapacity(Table *table, int capacity) {
     entries[i].value = NIL_VAL;
   }
 
+  table->count = 0;
+
   for (int i = 0; i < table->capacity; i++) {
     Entry *entry = &table->entries[i];
     if (entry->key == NULL)
@@ -59,6 +70,7 @@ static void adjustCapacity(Table *table, int capacity) {
     Entry *dest = findEntry(entries, capacity, entry->key);
     dest->key = entry->key;
     dest->value = entry->value;
+    table->count++;
   }
 
   FREE_ARRAY(Entry, table->entries, table->capacity);
@@ -75,12 +87,25 @@ bool tableSet(Table *table, ObjString *key, Value value) {
 
   Entry *entry = findEntry(table->entries, table->capacity, key);
   bool isNewKey = entry->key == NULL;
-  if (isNewKey)
+  if (isNewKey && IS_NIL(entry->value))
     table->count++;
 
   entry->key = key;
   entry->value = value;
   return isNewKey;
+}
+
+bool tableDelete(Table *table, ObjString *key) {
+  if (table->count == 0)
+    return false;
+
+  Entry *entry = findEntry(table->entries, table->capacity, key);
+  if (entry->key == NULL)
+    return false;
+
+  entry->key = NULL;
+  entry->value = BOOL_VAL(true);
+  return true;
 }
 
 void tableAddAll(Table *from, Table *to) {
