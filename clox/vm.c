ab2e0f5cc7e24070f218931fb215f4cf8640a204
Niall Antony <88824327+niallantony@users.noreply.github.com>
2026-02-24 23:44:18 +0900

    Add support for print statements

--- clox/vm.c
+++ clox/vm.c
@@ -148,9 +148,13 @@ static InterpretResult run() {
       push(NUMBER_VAL(-AS_NUMBER(pop())));
       break;
     }
-    case OP_RETURN: {
+    case OP_PRINT: {
       printValue(pop());
       printf("\n");
+      break;
+    }
+    case OP_RETURN: {
+      // exit interpreter
       return INTERPRET_OK;
     }
     }
